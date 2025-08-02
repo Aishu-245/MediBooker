2a51417dba3bebde9cc565e105cea3a99c2d707c
Aishu-245 <aishupulikanti245@gmail.com>
2025-08-02 21:08:31 +0530

    deploty repo fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'wouter';
+import { Switch, Route, Router } from 'wouter';
 import Landing from './pages/Landing.jsx';
 import DoctorProfile from './pages/DoctorProfile.jsx';
 import BookingForm from './pages/BookingForm.jsx';
@@ -8,12 +8,14 @@ import NotFound from './pages/NotFound.jsx';
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" component={Landing} />
-        <Route path="/doctor/:id" component={DoctorProfile} />
-        <Route path="/book/:id" component={BookingForm} />
-        <Route component={NotFound} />
-      </Switch>
+      <Router base="/MediBooker">
+        <Switch>
+          <Route path="/" component={Landing} />
+          <Route path="/doctor/:id" component={DoctorProfile} />
+          <Route path="/book/:id" component={BookingForm} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     );
   }
 }
